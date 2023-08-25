31b21383143a5de79951cae6aa17bfc245ef4f21
Guilherme Lage da costa <guilhermeldcosta@gmail.com>
2023-08-24 22:32:50 -0300

    feat: testes no Main

--- tdd_relogio/src/main/java/Main.java
+++ tdd_relogio/src/main/java/Main.java
@@ -1,5 +1,15 @@
+import classes.Relogio;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        Relogio hora1 = new Relogio(12, 20, 30);
+        Relogio hora2 = new Relogio(12, 50, 50);
+        Relogio relogio = new Relogio();
+
+        relogio.cronometrarIntervalo(hora1,hora2);
+        hora2.atualizarHoraAtual();
+        hora2.imprimir("AM/PM"); //valores aceitos: '24h' e 'AM/PM'
 
     }
 }
